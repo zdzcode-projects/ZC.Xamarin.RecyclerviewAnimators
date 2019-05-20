25c8c69027364118d98d6f9048c85be09986c7e3
Zdzislaw Kochanski <zdzislaw@zdzcode.com.br>
2019-05-20 09:03:24 -0300

    wasabeef/recyclerview-animators Version 3.0.0

---
+++ ClassLibrary1/Additions/BaseItemAnimator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using Android.Runtime;
+using Android.Support.V7.Widget;
+using Java.Interop;
+
+namespace JP.Wasabeef.Recyclerview.Animators
+{
+    public partial class BaseItemAnimator
+    {
+        BaseItemAnimatorInvoker _baseItemAnimatorInvoker;
+
+        public unsafe override bool AnimateAppearance(RecyclerView.ViewHolder viewHolder, ItemHolderInfo preLayoutInfo, ItemHolderInfo postLayoutInfo)
+        {
+            _baseItemAnimatorInvoker = new BaseItemAnimatorInvoker(this.Handle);    
+
+            _baseItemAnimatorInvoker.AnimateAppearance()
+
+            return JP.Wasabeef.Recyclerview.Animators.BaseItemAnimatorInvoker.AnimatePersistence(viewHolder, preLayoutInfo, postLayoutInfo);
+        }
+
+        public override bool AnimateChange(RecyclerView.ViewHolder oldHolder, RecyclerView.ViewHolder newHolder, ItemHolderInfo preLayoutInfo, ItemHolderInfo postLayoutInfo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool AnimateDisappearance(RecyclerView.ViewHolder viewHolder, ItemHolderInfo preLayoutInfo, ItemHolderInfo postLayoutInfo)
+        {
+            return AnimateDisappearance(viewHolder, preLayoutInfo, postLayoutInfo);
+        }
+
+        public override bool AnimatePersistence(RecyclerView.ViewHolder viewHolder, ItemHolderInfo preLayoutInfo, ItemHolderInfo postLayoutInfo)
+        {
+            return AnimatePersistence(viewHolder, preLayoutInfo, postLayoutInfo);
+        }
+    }
+}
